94a716beda28a254ccb2344b4bc577274347c20d
Sean McLeish <sean.mcl.de@gmail.com>
2019-02-04 17:08:22 +0100

    Add, Fix: [A] MEMORY USAGE TESTCASE, [A] PROCESS MEMORY TESTCASE, [E] ALLOCATING-SCRIPT

--- rm-sensor/AllocateMemory.sh
+++ rm-sensor/AllocateMemory.sh
@@ -1,5 +1,5 @@
-memAvailable=$(awk '/MemAvailable/{printf "%d\n", $2;}' < /proc/meminfo);
-memTotal=$(awk '/MemTotal/{printf "%d\n", $2;}' < /proc/meminfo);
-let "allocationSize=$memAvailable-$memTotal*$1";
+memAvailable=$(awk '/MemFree/{ printf "%f\n", $2}' /proc/meminfo);
+memTotal=$(awk '/MemTotal/{printf "%f\n", $2;}' < /proc/meminfo);
+allocationSize=$(echo "($memTotal * $1)-$memAvailable" | bc -l);
 echo "Allocating memory by $allocationSize KB";
 stress --vm-bytes "$allocationSize"K --vm-keep -m 1;
